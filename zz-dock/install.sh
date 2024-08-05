4df383351951b386c416dcad638915c3bd4c773f
Abdulrohim 'Him' Sama <abdulrohimhim.s@its-MacBook-Pro.local>
2024-08-05 15:21:48 +0700

    update dock settings

--- zz-dock/install.sh
+++ zz-dock/install.sh
@@ -2,12 +2,12 @@ brew install dockutil
 dockutil --no-restart --remove all
 # dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Sourcetree.app"
+# dockutil --no-restart --add "/Applications/Sourcetree.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Android Studio.app"
 dockutil --no-restart --add "/Applications/IntelliJ IDEA CE.app"
-dockutil --no-restart --add "/Applications/Slack.app"
+# dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 # dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Harvest.app"
+# dockutil --no-restart --add "/Applications/Harvest.app"
 killall Dock
